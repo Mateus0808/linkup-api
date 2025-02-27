1a96f38934815a80985f5c94ce35002a7224e1bd
Mateus dos Santos <loiolamateus7@gmail.com>
2025-02-27 13:50:44 -0300

    create docker compose e add packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './main/app.module';
+import { AlreadyExistsErrorFilter } from './app/errors/already-exists-error/already-exists-error.filter';
+import { BadResquestErrorFilter } from './app/errors/bad-request-error/bad-request-error.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: 422,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalFilters(
+    new AlreadyExistsErrorFilter(),
+    new BadResquestErrorFilter()
+  );
+
   await app.listen(3000);
 }
 bootstrap();
